bb586cebd3a8e36be4b38cec1e9c75779cdcd392
ViacheslavTsybulskiy <126974582+v0ts@users.noreply.github.com>
2025-07-30 01:59:19 +0300

    done

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "normalize.css";
+import { Header } from "./Components/Header/Header";
+import { Hero } from "./Components/Hero/Hero";
+import { Weather } from "./Components/Weather/Weather";
+import { News } from "./Components/News/news";
+import { Slider } from "./Components/Slider/Slider";
+import { Footer } from "./Components/Footer/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero></Hero>
+        <Weather></Weather>
+        <News></News>
+        <Slider></Slider>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
