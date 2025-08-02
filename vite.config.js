49888f3384b94543d3c6adba5c78675ac6c18bec
ViacheslavTsybulskiy <126974582+v0ts@users.noreply.github.com>
2025-08-02 16:29:49 +0300

    local server fix

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-	server: {
-		headers: {
-			'Content-Type': 'application/javascript',
-		},
-	},
 	plugins: [react()],
 })
