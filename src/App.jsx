3dff8d4eb98d2f8512db2149e86c47578a245e0a
ViacheslavTsybulskiy <126974582+v0ts@users.noreply.github.com>
2025-08-09 19:08:44 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,68 @@
-import "normalize.css";
-import { useEffect, useState } from "react";
-import { getWeather, getWeatherByCoords } from "./services/get-weather";
+import 'normalize.css'
+import { useEffect, useState } from 'react'
+import { getWeather } from './services/get-weather'
 
-import { Header } from "./Components/Header/Header";
-import { Hero } from "./Components/Hero/Hero";
-import { Weather } from "./Components/Weather/Weather";
-import { News } from "./Components/News/News";
-import { Slider } from "./Components/Slider/Slider";
-import { Footer } from "./Components/Footer/Footer";
-import { HeaderProvider } from "./Components/Header/HeaderContext";
+import { Footer } from './Components/Footer/Footer'
+import { Header } from './Components/Header/Header'
+import { HeaderProvider } from './Components/Header/HeaderContext'
+import { Hero } from './Components/Hero/Hero'
+import { News } from './Components/News/News'
+import { Slider } from './Components/Slider/Slider'
+import { Weather } from './Components/Weather/Weather'
 
 function App() {
-  const [keyword, setKeyword] = useState(null);
-  const [weatherData, setWeatherData] = useState(null);
+	const [keyword, setKeyword] = useState(null)
+	const [weatherData, setWeatherData] = useState(null)
 
-  useEffect(() => {
-    if (keyword) {
-      getWeather(keyword).then((data) => setWeatherData(data));
-    }
-  }, [keyword]);
+	useEffect(() => {
+		if (keyword) {
+			getWeather(keyword).then(data => setWeatherData(data))
+		}
+	}, [keyword])
 
-  useEffect(() => {
-    if (weatherData.length) {
-      localStorage.setItem("weatherData", JSON.stringify(weatherData));
-    }
-  }, [weatherData]);
+	useEffect(() => {
+		if (weatherData && weatherData.length) {
+			localStorage.setItem('weatherData', JSON.stringify(weatherData))
+		}
+	}, [weatherData])
 
-  const deleteCard = (id) => {
-    const newWeatherData = weatherData.filter((weather) => weather.id !== id);
-    setWeatherData(newWeatherData);
-  };
+	const deleteCard = id => {
+		const newWeatherData = weatherData.filter(weather => weather.id !== id)
+		setWeatherData(newWeatherData)
+	}
 
-  const refreshCard = (id, keyword) => {
-    let index = null;
+	const refreshCard = (id, keyword) => {
+		let index = null
 
-    for (let i = 0; i < weatherData.length; i++) {
-      if (weatherData[i].id === id) {
-        index = i;
-        break;
-      }
-    }
+		for (let i = 0; i < weatherData.length; i++) {
+			if (weatherData[i].id === id) {
+				index = i
+				break
+			}
+		}
 
-    getWeather(keyword).then((data) => {
-      const weatherDataCopy = [...weatherData];
-      weatherDataCopy[index] = data;
+		getWeather(keyword).then(data => {
+			const weatherDataCopy = [...weatherData]
+			weatherDataCopy[index] = data
 
-      setWeatherData(weatherDataCopy);
-    });
-  };
+			setWeatherData(weatherDataCopy)
+		})
+	}
 
-  return (
-    <>
-      <HeaderProvider>
-        <Header />
-        <main>
-          <Hero setKeyword={setKeyword}></Hero>
-          {weatherData ? <Weather weatherData={weatherData}></Weather> : null}
-          <News></News>
-          <Slider></Slider>
-        </main>
-        <Footer />
-      </HeaderProvider>
-    </>
-  );
+	return (
+		<>
+			<HeaderProvider>
+				<Header />
+				<main>
+					<Hero setKeyword={setKeyword}></Hero>
+					{weatherData ? <Weather weatherData={weatherData}></Weather> : null}
+					<News></News>
+					<Slider></Slider>
+				</main>
+				<Footer />
+			</HeaderProvider>
+		</>
+	)
 }
 
-export default App;
+export default App
--- src/Components/Weather/WeatherItem.jsx
+++ src/Components/Weather/WeatherItem.jsx
@@ -1,90 +1,82 @@
-import { useState, useEffect } from "react";
-import styles from "./Weather.module.scss";
+import styles from './Weather.module.scss'
 
-import refreshIcon from "./img/refresh.svg";
-import deleteIcon from "./img/delete.svg";
+import deleteIcon from './img/delete.svg'
+import refreshIcon from './img/refresh.svg'
 
-import { FaRegHeart as HeartIcon } from "react-icons/fa";
-import { FaHeart as FullHeartIcon } from "react-icons/fa6";
+import { FaRegHeart as HeartIcon } from 'react-icons/fa'
 
-export function WeatherItem({
-  data,
-  id,
-  deleteCard,
-  refreshCard,
-}) {
+export function WeatherItem({ data, id, deleteCard, refreshCard }) {
+	return (
+		<li className={styles.item} id={id}>
+			<div className={styles.textFlex}>
+				<p className={styles.text}>{data.city}</p>
+				<p className={styles.text}>{data.country}</p>
+			</div>
 
-  return (
-    <li className={styles.item} id={id}>
-      <div className={styles.textFlex}>
-        <p className={styles.text}>{data.city}</p>
-        <p className={styles.text}>{data.country}</p>
-      </div>
+			<p className={styles.time}>
+				{data.hours}:{data.minutes}
+			</p>
 
-      <p className={styles.time}>
-        {data.hours}:{data.minutes}
-      </p>
+			<ul className={styles.buttonsList}>
+				<li>
+					<button className={styles.button}>Hourly forecast</button>
+				</li>
+				<li>
+					<button className={styles.button}>Weekly forecast</button>
+				</li>
+			</ul>
 
-      <ul className={styles.buttonsList}>
-        <li>
-          <button className={styles.button}>Hourly forecast</button>
-        </li>
-        <li>
-          <button className={styles.button}>Weekly forecast</button>
-        </li>
-      </ul>
+			<div className={styles.dateFlex}>
+				<p className={styles.text}>
+					{data.dayDate}.{data.month}.{data.year}
+				</p>
+				<div className={styles.separator}></div>
+				<p className={styles.text}>{data.day}</p>
+			</div>
 
-      <div className={styles.dateFlex}>
-        <p className={styles.text}>
-          {data.dayDate}.{data.month}.{data.year}
-        </p>
-        <div className={styles.separator}></div>
-        <p className={styles.text}>{data.day}</p>
-      </div>
+			<img src={data.iconSrc} alt='weather icon' className={styles.img} />
 
-      <img src={data.iconSrc} alt="weather icon" className={styles.img} />
+			<h2 className={styles.temp}>{data.temp}℃</h2>
 
-      <h2 className={styles.temp}>{data.temp}℃</h2>
+			<div className={styles.iconsFlex}>
+				<svg className={styles.icon} onClick={() => refreshCard(id, data.city)}>
+					<use
+						className={styles.iconMobile}
+						href={refreshIcon}
+						width='24'
+						height='24'
+					></use>
+					<use
+						className={styles.iconDesktop}
+						href={refreshIcon}
+						width='30'
+						height='30'
+					></use>
+				</svg>
+				<HeartIcon
+					className={styles.iconFav}
+					onClick={() => {
+						setFav(true)
+						setFavName(data.city)
+					}}
+				/>
 
-      <div className={styles.iconsFlex}>
-        <svg className={styles.icon} onClick={() => refreshCard(id, data.city)}>
-          <use
-            className={styles.iconMobile}
-            href={refreshIcon}
-            width="24"
-            height="24"
-          ></use>
-          <use
-            className={styles.iconDesktop}
-            href={refreshIcon}
-            width="30"
-            height="30"
-          ></use>
-        </svg>
-        <HeartIcon
-          className={styles.iconFav}
-          onClick={() => {
-            setFav(true);
-            setFavName(data.city);
-          }}
-        />
-
-        <button className={styles.button}>See more</button>
-        <svg className={styles.icon} onClick={() => deleteCard(id)}>
-          <use
-            className={styles.iconMobile}
-            href={deleteIcon}
-            width="24"
-            height="24"
-          ></use>
-          <use
-            className={styles.iconDesktop}
-            href={deleteIcon}
-            width="30"
-            height="30"
-          ></use>
-        </svg>
-      </div>
-    </li>
-  );
+				<button className={styles.button}>See more</button>
+				<svg className={styles.icon} onClick={() => deleteCard(id)}>
+					<use
+						className={styles.iconMobile}
+						href={deleteIcon}
+						width='24'
+						height='24'
+					></use>
+					<use
+						className={styles.iconDesktop}
+						href={deleteIcon}
+						width='30'
+						height='30'
+					></use>
+				</svg>
+			</div>
+		</li>
+	)
 }
